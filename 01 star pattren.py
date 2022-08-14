2a2ef332e9d3119af9f4621b52f3168b548be520
NjFaiaz <47560573+njfaiaz@users.noreply.github.com>
2022-08-14 14:51:35 +0600

    Create 01 star pattren.py

---
+++ 01 star pattren.py
@@ -0,0 +1,6 @@
+a = int(input("Enter Your Number: "))
+
+for i in range(1, a + 1): # Line ta print korbe 
+    for j in range(1, i + 1): # * ba value ta print korbe
+        print("* ", end=(""))
+    print()
