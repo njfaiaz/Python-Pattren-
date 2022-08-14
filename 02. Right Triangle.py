45f110a000bb095c78e0241d39364bb1164cd81a
NjFaiaz <47560573+njfaiaz@users.noreply.github.com>
2022-08-14 15:16:30 +0600

    Update 02. Right Triangle.py

--- 02. Right Triangle.py
+++ 02. Right Triangle.py
@@ -4,3 +4,11 @@ for i in range(1, a + 1):
         for j in range(1, i+1):
             print("* ", end=(""))
     print()
+
+    
+a = int(input("Enter a value: "))
+for i in range(1, a + 1):
+    if i%2 != 1: # Even number print 
+        for j in range(1, i+1):
+            print("* ", end=(""))
+        print()
