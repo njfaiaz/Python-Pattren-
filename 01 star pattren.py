0e9dcf20d39c584089d0978d9b12f65035f5fac9
NjFaiaz <47560573+njfaiaz@users.noreply.github.com>
2022-08-14 14:55:25 +0600

    Update 01 star pattren.py

--- 01 star pattren.py
+++ 01 star pattren.py
@@ -1,4 +1,4 @@
-a = int(input("Enter Your Number: "))
+a = int(input("Enter a value: "))
 
 for i in range(1, a + 1): # Line ta print korbe 
     for j in range(1, i + 1): # * ba value ta print korbe
