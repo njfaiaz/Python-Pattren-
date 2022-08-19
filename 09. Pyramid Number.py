ec44c87003b3072536c3d50ebc3712dd4b0c2609
NjFaiaz <47560573+njfaiaz@users.noreply.github.com>
2022-08-19 19:33:26 +0600

    Create 09. Pyramid Number.py

---
+++ 09. Pyramid Number.py
@@ -0,0 +1,15 @@
+a = int(input("Enter a value:  "))
+
+for i in range(1,a+1):
+    print(" "*(a-i), end="")
+    for j in range(1,i+1):
+        print(j, " ",end="")
+    print()
+
+a = int(input("Enter a value:  "))
+
+for i in range(1,a+1):
+    print(" "*(a-i), end="")
+    for j in range(1,i+1):
+        print(i, " ",end="")
+    print()
