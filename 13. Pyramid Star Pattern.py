7da26839aca407bd2ee0d20ec12209404d58e700
NjFaiaz <47560573+njfaiaz@users.noreply.github.com>
2022-08-20 19:36:47 +0600

    Update 13. Pyramid Star Pattern.py

--- 13. Pyramid Star Pattern.py
+++ 13. Pyramid Star Pattern.py
@@ -1,9 +1,9 @@
-a = int(input("Enter a value: "))
+a = int(input("Enter a value: ")) # a= 5
 
 k = 1
 
-for i in range(1, a+1):
-    print(" "*(a-i), end="")
+for i in range(1, a+1):  # 1,2,3,4,5
+    print(" "*(a-i), end="")# 5-1=4, ba a-i=4
     for j in range(1, k+1):
         print("*", end="")
     print()
