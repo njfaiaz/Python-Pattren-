fbc4bf09adc49ee13b79e967a5eba896cce36a7b
NjFaiaz <47560573+njfaiaz@users.noreply.github.com>
2022-08-21 20:04:36 +0600

    Update 17. Mirrored Half Diamond .py

--- 17. Mirrored Half Diamond .py
+++ 17. Mirrored Half Diamond .py
@@ -1,13 +1,21 @@
-a = int(input("Enter a value: "))
+a = int(input("Enter a value: ")) #a = 5
 
-for i in range(1,a+1):
-    print(" "*(a-i),end="")
-    for j in range(1,i+1):
-        print("*",end="")
+for i in range(1,a+1): # 1,2,3,4,5
+    for j in range(1, i+1):
+        print("*", end="")
     print()
 
-for i in range((a-1),0,-1):
-    print(" "*(a-i),end="")
+for i in range((a-1),0, -1):
     for j in range(1,i+1):
-        print("*",end="")
+        print("*", end="")
     print()
+
+
+
+b = int(input("Enter a value: "))
+
+for i in range(1,b+1):
+    print("*"*i)
+
+for j in range((b-1),0, -1):
+    print("*"*j)
