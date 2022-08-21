02df108180d45cb080d13ef1acea10aaec928de2
NjFaiaz <47560573+njfaiaz@users.noreply.github.com>
2022-08-21 19:28:23 +0600

    Create 16. Half Diamond.py

---
+++ 16. Half Diamond.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a value: ")) #a = 5
+
+for i in range(1,a+1): # 1,2,3,4,5
+    for j in range(1, i+1):
+        print("*", end="")
+    print()
+
+for i in range((a-1),0, -1):
+    for j in range(1,i+1):
+        print("*", end="")
+    print()
