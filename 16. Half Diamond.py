f729bba48decef832a200e8bc46b3dd19edb44e1
NjFaiaz <47560573+njfaiaz@users.noreply.github.com>
2022-08-21 19:38:38 +0600

    Update 16. Half Diamond.py

--- 16. Half Diamond.py
+++ 16. Half Diamond.py
@@ -9,3 +9,13 @@ for i in range((a-1),0, -1):
     for j in range(1,i+1):
         print("*", end="")
     print()
+
+
+
+b = int(input("Enter a value: "))
+
+for i in range(1,a+1):
+    print("*"*i)
+
+for j in range((a-1),0, -1):
+    print("*"*j)
